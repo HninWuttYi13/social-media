60983345985fdeddd1f53153561c51d0d37cc4cc
Hnin Wutt Yi <hninwuttyi.mgn@gmail.com>
2025-10-11 20:49:05 +0630

    modifying postseeder to insert image by using createMany and run seed

--- SocialMedia-Api/prisma/seeds/PostSeeder.js
+++ SocialMedia-Api/prisma/seeds/PostSeeder.js
@@ -2,15 +2,25 @@ const {PrismaClient } = require("@prisma/client");
 const {faker} = require("@faker-js/faker");
 const prisma = new PrismaClient();
 async function PostSeeder(){
-    const data = [];
+    console.log("Post Seeding is started");
     for(let i =0 ; i <20; i++){
         const content = faker.lorem.paragraph();
-        const images = [faker.image.urlLoremFlickr(), faker.image.urlLoremFlickr()];
+        const imageURLs = [faker.image.urlLoremFlickr(), faker.image.urlLoremFlickr()];
         const userId = faker.number.int({min: 1, max: 10});
-        data.push({content, images, userId})
+        await prisma.post.create({
+            data: {
+                content, 
+                userId,
+                images: {
+                    createMany: {
+                        data: imageURLs.map(url => ({
+                            imageUrl: url
+                        }))
+                    }
+                }
+            }
+        })
     }
-    console.log("Post Seeding is started");
-    await prisma.post.createMany({data});
     console.log("Post Seeding done");
     
    

443fbca658578c865a839dabf5f0c11a1994fb17
Hnin Wutt Yi <hninwuttyi.mgn@gmail.com>
2025-10-12 19:09:52 +0630

    create the routes to find the users

--- SocialMedia-Api/routes/content.js
+++ SocialMedia-Api/routes/content.js
@@ -1,41 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const prisma = require("../prismaClient");
-router.get("/posts", async(req, res)=> {
-    try {
-        const data = await prisma.post.findMany({
-            include: {
-                user: true,
-                comments: true,
-                images: true
-                
-            },
-            orderBy: {id:"desc"},
-            take: 20
-        });
-        res.json(data)
-    } catch (e) {
-            res.status(500).json({error: e})
-        
-    }
+router.get("/posts", async (req, res) => {
+  try {
+    const data = await prisma.post.findMany({
+      include: {
+        user: true,
+        comments: true,
+        images: true,
+      },
+      orderBy: { id: "desc" },
+      take: 20,
+    });
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 });
-router.get("/posts/:id", async(req, res)=> {
-    const { id } = req.params;
-    try {
-        const data = await prisma.post.findFirst({
-          where: { id: Number(id) },
-          include: {
-            user: true,
-            images: true,
-            comments: {
-              include: { user: true },
-            },
-          },
-        });
-        res.json(data);
-        
-    } catch (e) {
-        res.status(500).json({error: e})
-    }
-})
-module.exports = {contentRouter: router}
+router.get("/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await prisma.post.findFirst({
+      where: { id: Number(id) },
+      include: {
+        user: true,
+        images: true,
+        comments: {
+          include: { user: true },
+        },
+      },
+    });
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
+});
+router.get("/users", async (req, res) => {
+  try {
+    const data = await prisma.user.findMany({
+      include: {
+        posts: true,
+        comments: true,
+      },
+      orderBy: { id: "desc" },
+      take: 20,
+    });
+    res.json(data)
+  } catch (e) {
+    res.status(500).json({error: e})
+  }
+});
+module.exports = { contentRouter: router };
